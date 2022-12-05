c9cd6560e9d4a21eda618383e2bb7e59dde79993
gary003 <gary.johnson003@gmail.com>
2022-12-05 20:21:40 +0100

    add returnType utilityType

--- utilityTypes.ts
+++ utilityTypes.ts
@@ -5,8 +5,8 @@ export enum currencyEnum {
 
 export type currencyList = "hardCurrency" | "softCurrency"
 
-export type currencyTypes = Record<currencyList, number>
-// export type currencyTypes = Record<currencyEnum, string | number>
+// export type currencyTypes = Record<currencyList, string | number>
+export type currencyTypes = Record<currencyEnum, string | number>
 
 export type walletDTO = { walletId: string } & Partial<currencyTypes>
 
@@ -39,3 +39,56 @@ const playerOne: playerDTO = {
 }
 
 console.log(playerOne)
+
+export type anonymousUser = Omit<userDTO, "firstname" | "lastname">
+
+const playerTwo: playerDTO = {
+  userId: "1209876",
+  firstname: "Glen",
+  lastname: "Rhee",
+  // previousOccupation: "pizza delivery",
+  gamerLevel: 3
+} as playerDTO
+
+console.log(playerTwo)
+
+const characteres: playerDTO[] = [
+  {
+    userId: "1209876",
+    firstname: "Beth",
+    lastname: "Greene",
+    // previousOccupation: "pizza delivery",
+    gamerLevel: 13
+  },
+  {
+    userId: "63536282",
+    firstname: "Glen",
+    lastname: "Rhee",
+    // previousOccupation: "pizza delivery",
+    gamerLevel: 10
+  },
+  {
+    userId: "09128373",
+    firstname: "Carl",
+    lastname: "Grimes",
+    // previousOccupation: "pizza delivery",
+    gamerLevel: 3
+  },
+  {
+    userId: "298373633",
+    firstname: "Rosita",
+    lastname: "espinosa",
+    // previousOccupation: "pizza delivery",
+    gamerLevel: 14
+  }
+]
+
+const getTopPlayer = (players: playerDTO[]) => {
+  return players.reduce((acc, player) => {
+    return player.gamerLevel > acc.gamerLevel ? player : acc
+  })
+}
+
+console.log(getTopPlayer(characteres))
+
+export type getTopPlayerType = ReturnType<typeof getTopPlayer>
